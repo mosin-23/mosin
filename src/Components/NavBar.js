d566242a4c97aa58158c48183b4b84ef925eff4b
Abdulmosinkhanpathan <114325519+Abdulmosinkhanpathan@users.noreply.github.com>
2024-09-05 16:36:31 +0530

    Prefinal commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,62 +1,81 @@
 import React, { useState } from "react";
-import mk from '../assets/mk.png'
+import mk from '../assets/mk.png';
 import "./NavBar.css";
+
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
+
   return (
     <div className="w-full p-[10px] flex justify-between items-center">
-      <img src={mk} className="h-[60px] drop-shadow-md hover:drop-shadow-xl"  onClick={()=>{window.location.reload()}}/>
+      <img src={mk} className="h-[60px] drop-shadow-md hover:drop-shadow-xl" onClick={() => window.location.reload()} />
+
       <div>
         <ul className="hidden md:flex">
           <li className="menuItem hover:scale-110 hover:z-50 transition-all duration-300 ease-in-out">
-            <a href="home">Home</a>
+            <a href="#home">Home</a>
           </li>
           <li className="menuItem hover:scale-110">
-            <a href="aboutme">About Me</a>
+            <a href="#aboutme">About Me</a>
           </li>
           <li className="menuItem hover:scale-110">
-            <a href="skills">Skills</a>
+            <a href="#skills">Skills</a>
           </li>
           <li className="menuItem hover:scale-110">
-            <a href="certifications">Certifications</a>
+            <a href="#certifications">Certifications</a>
           </li>
           <li className="menuItem hover:scale-110">
-            <a href="contact">Contact Us</a>
+            <a href="#contact">Contact Us</a>
           </li>
         </ul>
-        {/* <img src={logo} className="sm:hidden w-[40px] h-[40px]" /> */}
-        <div className="flex flex-col  items-end">
-      {!toggle?  <svg xmlns="http://www.w3.org/2000/svg" fill="none" onClick={()=>setToggle(!toggle)}
-        viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-         className="w-6 h-6 cursor-pointer md:hidden">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-</svg>:<svg onClick={()=>setToggle(!toggle)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 cursor-pointer md:hidden h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-</svg>
-}
 
-{toggle?
+        {/* Mobile Menu */}
+        <div className="flex flex-col items-end">
+          {!toggle ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              onClick={() => setToggle(!toggle)}
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 cursor-pointer md:hidden"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+            </svg>
+          ) : (
+            <svg
+              onClick={() => setToggle(!toggle)}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 cursor-pointer md:hidden h-6"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          )}
+
+          {toggle && (
             <ul className="md:hidden absolute flex mt-8 flex-col bg-gray-200 p-2 rounded-sm">
-          <li className="menuItem p-3 hover:scale-110 hover:z-50 transition-all duration-300 ease-in-out">
-            <a href="home">Home</a>
-          </li>
-          <li className="menuItem  p-3 hover:scale-110">
-            <a href="aboutme">About Me</a>
-          </li>
-          <li className="menuItem p-3 hover:scale-110">
-            <a href="skills">Skills</a>
-          </li>
-          <li className="menuItem p-3 hover:scale-110">
-            <a href="certifications">Certifications</a>
-          </li>
-          <li className="menuItem p-3 hover:scale-110">
-            <a href="contact">Contact Us</a>
-          </li>
-        </ul>:null}
+              <li className="menuItem p-3 hover:scale-110 hover:z-50 transition-all duration-300 ease-in-out">
+                <a href="#home">Home</a>
+              </li>
+              <li className="menuItem p-3 hover:scale-110">
+                <a href="#aboutme">About Me</a>
+              </li>
+              <li className="menuItem p-3 hover:scale-110">
+                <a href="#skills">Skills</a>
+              </li>
+              <li className="menuItem p-3 hover:scale-110">
+                <a href="#certifications">Certifications</a>
+              </li>
+              <li className="menuItem p-3 hover:scale-110">
+                <a href="#contact">Contact Us</a>
+              </li>
+            </ul>
+          )}
         </div>
-        {/* <ul className={!toggle?'hidden':'flex'}>
-                LOGO
-            </ul> */}
       </div>
     </div>
   );
