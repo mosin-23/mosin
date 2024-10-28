991d32b180dd7cf8412032f17233ad955e5713d0
Abdulmosinkhanpathan <114325519+Abdulmosinkhanpathan@users.noreply.github.com>
2024-10-28 21:39:19 +0530

    Updated AboutMe component with new content

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,12 +7,12 @@ const AboutMe = () => {
   return (
     <div id="aboutme" className='mt-[100px]'>
           <img src={wave}  className="w-screen absolute " />
-        <div className='w-full bg-gradient-to-r from-emerald-500 to-emerald-900 h-[300px]  justify-center items-center'>
+        <div className='w-full bg-gradient-to-r from-emerald-500 to-emerald-900 lg:h-[350px] md:h-[400px] sm:h-[1000px] justify-center items-center'>
         <div className='flex flex-row justify-between absolute'>
             <img src={laptop}  className="hidden lg:block lg:w-[250px]  mt-[-100px]" />
         </div>
-            <h1 className='text-white lg:text-[50px] pt-[50px] font-bold text-center md:text-sm sm:text-sm'>About <span className='text-[#F2EFC7] mt-10'>Me</span></h1>
-            <h1 className="w-auto px-10 lg:text-xl lg:font-sans lg:text-justify  md:text-base sm:text-xs lg:px-48 mt-[50px] text-white py-0 leading-relaxed pb-1">
+            <h1 className='text-white lg:text-[50px] pt-[50px] font-bold text-center md:text-lg sm:text-base'>About <span className='text-[#F2EFC7] mt-10'>Me</span></h1>
+              <h1 className=" w-auto p-5 py-6 mb-10 text-center lg:text-xl lg:font-sans lg:text-justify  lg:px-48 mt-[50px] text-white leading-relaxed pb-1">
   {AppText.aboutMeDescripion}
 </h1>
 
